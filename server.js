3826859396f7f0f3b023aa1016f86d2cf46a3499
Hafizjunaid <Hafizjunaid971@gmail.com>
2023-07-07 01:00:01 +0500

    login token

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
-
-
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 //confugure
 dotenv.config();
+//databse config
+connectDB();
 //rest project
 const app = express()
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+
 
 
 //rest api
