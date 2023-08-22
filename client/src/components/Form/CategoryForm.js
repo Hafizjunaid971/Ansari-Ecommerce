47578432a4da897a1a9fc7457fb86d3b72f411ad
Hafizjunaid <Hafizjunaid971@gmail.com>
2023-08-23 01:21:27 +0500

    product

--- client/src/components/Form/CategoryForm.js
+++ client/src/components/Form/CategoryForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const CategoryForm = () => {
+const CategoryForm = ({ handleSubmit, value, setValue }) => {
   return (
-    <div>CategoryForm</div>
-  )
-}
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter new category"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
 
-export default CategoryForm
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CategoryForm;
